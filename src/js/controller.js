bd0c929306fc3f95daade4ffd319c0b2e83af67f
Suyeon <pisuyeun@gmail.com>
2021-07-17 22:19:24 +0900

    Implement DOM updating

--- src/js/controller.js
+++ src/js/controller.js
@@ -14,6 +14,9 @@ const controlRecipe = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // 1) Loading recipe
     await model.loadRecipe(id);
 
@@ -58,7 +61,7 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
