f24b64a9e640b6fe7929b6ef3df2b6854faec301
Suyeon <pisuyeun@gmail.com>
2021-07-17 22:36:43 +0900

    Fix bug in loadSearchResults

--- src/js/model.js
+++ src/js/model.js
@@ -45,6 +45,7 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err}🚫🚫`);
     throw err;
