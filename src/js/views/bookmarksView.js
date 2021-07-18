be5d338c61dc26cf51a5fe7962847f55d82721b0
Suyeon <pisuyeun@gmail.com>
2021-07-18 17:13:23 +0900

    Refector bookmarks

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,5 +1,5 @@
 import View from './View.js';
-
+import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 
 class BookmarksView extends View {
@@ -8,25 +8,9 @@ class BookmarksView extends View {
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(results) {
-    const id = window.location.hash.slice(1);
-    return `
-    <li class="preview">
-      <a class="preview__link ${
-        results.id === id ? 'preview__link--active' : ''
-      }" href="#${results.id}">
-        <figure class="preview__fig">
-          <img src="${results.image}" alt="${results.title}" />
-        </figure>
-        <div class="preview__data">
-          <h4 class="preview__title">${results.title}</h4>
-          <p class="preview__publisher">${results.publisher}</p>
-        </div>
-      </a>
-    </li>`;
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
   }
 }
 
